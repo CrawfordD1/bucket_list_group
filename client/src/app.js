e072bbe7470f99bb0d5a43f3280646a8bcd7f088
Crawford Davidson <crawforddavidson@gmail.com>
2017-08-09 15:00:02 +0100

    UI now takes in country DB

--- client/src/app.js
+++ client/src/app.js
@@ -1,17 +1,28 @@
+var UI = require('./views/ui.js');
+
 var init = function(){
 
-var url = "https://restcountries.eu/rest/v2/all"
-var countries = []
-getCountriesFromAPI(url);
+  var urldb = "http://localhost:3000/bucketlist"
+  makeDataRequest(urldb, datarequestComplete);
 
-var countryselect = document.getElementById("country-selection");
-countryselect.addEventListener('change', function(){
+  var url = "https://restcountries.eu/rest/v2/all"
+  var countries = []
+  getCountriesFromAPI(url);
 
-  if(this.value !== "none"){
+  var countryselect = document.getElementById("country-selection");
+  countryselect.addEventListener('change', function(){
 
-  var url = "http://localhost:3000/bucketlist";
-  var method = "POST";
-  var postData = this.value;
+    if(this.value !== "none"){
+
+      document.getElementById('country-selection').onchange = (function() {
+        document.getElementsByTagName('audio')[0].play();
+        return false;
+      });
+
+
+      var url = "http://localhost:3000/bucketlist";
+      var method = "POST";
+      var postData = this.value;
   // var postData = JSON.parse(this.value);
 
   var async = true;
@@ -21,16 +32,33 @@ countryselect.addEventListener('change', function(){
   request.onload = function () {
      var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
      var data = request.responseText; // Returned data, e.g., an HTML document.
-  }
+   }
 
-  request.open(method, url, async);
+   request.open(method, url, async);
 
-  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
 
-  request.send(postData);
-}
+   request.send(postData);
+ }
 })
 }
+
+
+
+
+var makeDataRequest = function(url, callback){
+var request = new XMLHttpRequest();
+request.open('GET', url);
+request.addEventListener('load', callback);
+request.send();
+};
+
+var datarequestComplete = function(){
+if(this.status !== 200)return;
+var countryString = this.responseText;
+var countries = JSON.parse(countryString);
+var ui = new UI(countries);
+};
 
 var getCountriesFromAPI = function(url){
   makeRequest(url, requestComplete);
--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,24 +1,23 @@
 var UI = function(countries){
-  this.render(countries);
+    this.render(countries);
 }
 
 UI.prototype = {
   render: function(countries){
-
     console.log(countries);
     countries.forEach( function(country){
-      var mapDiv = document.createElement('div');
+      // var mapDiv = document.createElement('div');
       var img = document.createElement('img');
       var li = document.createElement('li');
       var ul = document.getElementById('countries');
 
-      var center = {lat: country.latlng[0]. lng: country.latlng[1]};
-      var zoom = 10;
-      var mainMap = new MapWrapper(mapDiv, center, zoom);
+      // var center = {lat: country.latlng[0]. lng: country.latlng[1]};
+      // var zoom = 10;
+      // var mainMap = new MapWrapper(mapDiv, center, zoom);
 
       img.src = country.flag;
       li.innerText = country.name;
-      img.appendChild(mapDiv);
+      // img.appendChild(mapDiv);
       li.appendChild(img);
       ul.appendChild(li);
     })
