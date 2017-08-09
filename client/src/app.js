961370fbd79273ebb169393a153601d34516097a
Crawford Davidson <crawforddavidson@gmail.com>
2017-08-09 14:23:29 +0100

    updated app.js to post request on selection change

--- client/src/app.js
+++ client/src/app.js
@@ -3,6 +3,33 @@ var init = function(){
 var url = "https://restcountries.eu/rest/v2/all"
 var countries = []
 getCountriesFromAPI(url);
+
+var countryselect = document.getElementById("country-selection");
+countryselect.addEventListener('change', function(){
+
+  if(this.value !== "none"){
+
+  var url = "http://localhost:3000/bucketlist";
+  var method = "POST";
+  var postData = this.value;
+  // var postData = JSON.parse(this.value);
+
+  var async = true;
+
+  var request = new XMLHttpRequest();
+
+  request.onload = function () {
+     var status = request.status; // HTTP response status, e.g., 200 for "200 OK"
+     var data = request.responseText; // Returned data, e.g., an HTML document.
+  }
+
+  request.open(method, url, async);
+
+  request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+  request.send(postData);
+}
+})
 }
 
 var getCountriesFromAPI = function(url){
@@ -25,10 +52,15 @@ var requestComplete = function(){
 
 var populateCountrySelector = function(countries){
   var selector = document.getElementById("country-selection");
+  var option = document.createElement('option');
+  option.innerText = "Select a country to add";
+  option.value = "none";
+  selector.appendChild(option);
+
   for(country of countries){
     var option = document.createElement('option');
     option.innerText = country.name;
-    option.value = country;
+    option.value = JSON.stringify(country);
     selector.appendChild(option);
   }
 }
