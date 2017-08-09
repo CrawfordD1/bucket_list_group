978d044b42c4d7f616783620637b07f2339c2faf
Crawford Davidson <crawforddavidson@gmail.com>
2017-08-09 15:17:56 +0100

    fixed google map

--- client/src/views/ui.js
+++ client/src/views/ui.js
@@ -1,3 +1,5 @@
+var MapWrapper = require('./mapWrapper.js')
+
 var UI = function(countries){
     this.render(countries);
 }
@@ -6,18 +8,20 @@ UI.prototype = {
   render: function(countries){
     console.log(countries);
     countries.forEach( function(country){
-      // var mapDiv = document.createElement('div');
+      var mapDiv = document.createElement('div');
       var img = document.createElement('img');
       var li = document.createElement('li');
       var ul = document.getElementById('countries');
 
-      // var center = {lat: country.latlng[0]. lng: country.latlng[1]};
-      // var zoom = 10;
-      // var mainMap = new MapWrapper(mapDiv, center, zoom);
+      mapDiv.style.height = '200px';
+
+      var center = {lat: country.latlng[0], lng: country.latlng[1]};
+      var zoom = 10;
+      var mainMap = new MapWrapper(mapDiv, center, zoom);
 
       img.src = country.flag;
       li.innerText = country.name;
-      // img.appendChild(mapDiv);
+      li.appendChild(mapDiv);
       li.appendChild(img);
       ul.appendChild(li);
     })
